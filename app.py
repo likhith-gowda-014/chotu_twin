ae0204c13902bfb0046baa6a47576f5182539a96
likhith-gowda-014 <likhithgowda6090@gmail.com>
2025-04-11 11:20:02 +0530

    Removed the port 0.0.0.0

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def tts_audio():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ["PORT"]))
+    app.run(debug=True)
 
